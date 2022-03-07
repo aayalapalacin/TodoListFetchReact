13a64ff3b1d7498532a2881c7a943014e2a1b5f7
avokeys <alexander.ayala.palacin@gmail.com>
2022-03-07 19:16:28 +0000

    fetched API to populate todo list, now need to update api when item is removed

--- src/js/component/UserInput.js
+++ src/js/component/UserInput.js
@@ -3,19 +3,21 @@ import React, { useState, useEffect } from "react";
 
 // function that will contain everything needed to let the todo list run
 export const UserInput = () => {
-	const getAllTodos = () => {
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/alexAyala")
-			.then((response) => response.json())
-			.then((data) => setVariable(data));
-	};
-
-	useEffect(() => {
-		getAllTodos();
-	}, []);
+	// state that will contain the first input where you initially enter a todo item
+	// before pressing enter
+	// listItem starting value (empty string)
+	// setListItem allows the input value to be passed to new array and clear the
+	// first input after pressing enter
+	const [listItem, setListItem] = useState("");
+	// todoList is an empty array created to put a new todo item's string inside
+	// setTodoList will be used to update the empty array to the new string value AND
+	//  clear the array when x div is clicked
+	const [todoList, setTodoList] = useState([]);
 
 	const userURL =
-			"https://assets.breatheco.de/apis/fake/todos/user/alexAyala";
+		"https://assets.breatheco.de/apis/fake/todos/user/alexAyala";
 
+	const getAllTodos = () => {
 		fetch(userURL)
 			.then((response) => {
 				if (!response.ok) {
@@ -25,52 +27,63 @@ export const UserInput = () => {
 				}
 				return response.json();
 			})
-			.then((data) => setVariable(data))
+			.then((data) => setTodoList(data))
 			.catch((err) => console.error(err));
+	};
+
+	const remove = (index) => {
+		const removeItem = todoList.filter((item, i) => i != index);
+		setTodoList(removeItem);
+		console.log("todo list test", todoList);
+		console.log("remove item", removeItem);
+
+		fetch(userURL, {
+			method: "PUT",
+			body: removeItem,
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				console.log("Success:", result);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+			});
+	};
+
+	// this is a function to spark an event when enter is pressed
+	const handleKeyPress = (event) => {
+		if (event.keyCode === 13) {
+			//todoList is an empty array. listItem is the value of what we type in
+			// initial input thanks to line __
+			const newTodoList = [...todoList, listItem];
+			// newTodoItem would look like [[],"initial string"]
+			// BUT since we used spread operator (...) it gets rid of the array inside array and
+			// give us: ["initial string"] just the array with string inside.
+			setTodoList(newTodoList);
+			// next the todoList is reset as this array. the value of the initial input is
+			setListItem("");
+			// set to empty string to clear initial input
+		}
 	};
 
 	useEffect(() => {
 		getAllTodos();
 	}, []);
-	// state that will contain the first input where you initially enter a todo item
-	// before pressing enter
-	// listItem starting value (empty string)
-	// setListItem allows the input value to be passed to new array and clear the
-	// first input after pressing enter
-	const [listItem, setListItem] = useState("");
-	// variable is an empty array created to put a new todo item's string inside
-	// setVariable will be used to update the empty array to the new string value AND
-	//  clear the array when x div is clicked
-	const [variable, setVariable] = useState([]);
-	// this is a function to spark an event when enter is pressed
-	const todoItem = (event) => {
-		if (event.keyCode === 13) {
-			//variable is an empty array. listItem is the value of what we type in
-			// initial input thanks to line __
-			const newTodoItem = [...variable, listItem];
-			// newTodoItem would look like [[],"initial string"]
-			// BUT since we used spread operator (...) it gets rid of the array inside array and
-			// give us: ["initial string"] just the array with string inside.
-			setVariable(newTodoItem);
-			// next the variable is reset as this array. the value of the initial input is
-			setListItem("");
-			// set to empty string to clear initial input
-		}
-	};
-	// variable is an array that looks like ["first item to do", "second item to do", "third item to do"]
-	// we are mapping this array called variable so we can maniupulate it to make it more like:
+
+	// todoList is an array that looks like ["first item to do", "second item to do", "third item to do"]
+	// we are mapping this array called todoList so we can maniupulate it to make it more like:
 	// ["first item to do"] ["second item to do"] ["third item to do"]
 	// then we will render the value of each array, which will become the new todo items on the userinterface
-	const todo = variable.map((item, index) => {
+	const todo = todoList.map((item, index) => {
 		console.log("item", item);
 		console.log("index", index);
-		console.log("variable", variable);
+		console.log("todoList", todoList);
 		return (
-			// the item is the string item of the array called variable such as: ["first thing"] (first thing =item)
+			// the item is the string item of the array called todoList such as: ["first thing"] (first thing =item)
 			// looks like key={index} allows you to choose a different item in array
 			// array is set up like: ["this is index 1", "this is index 2"]
 			<li className="list-group-item item" key={index}>
-				{item}
+				{item.label}
 
 				<div className="mouseOver" onClick={() => remove(index)}>
 					x
@@ -79,18 +92,13 @@ export const UserInput = () => {
 		);
 	});
 
-	const remove = (index) => {
-		const removeItem = variable.filter((item, i) => i != index);
-		setVariable(removeItem);
-	};
-
 	return (
 		<div>
 			<div>
 				<input
 					type="text"
 					className="item userInput"
-					onKeyDown={todoItem}
+					onKeyDown={handleKeyPress}
 					value={listItem}
 					onChange={(e) => setListItem(e.target.value)}
 					placeholder="What needs to be done?"
